56881f0ae19219bd6a4573ae108281d908647350
RajlaxmiSen <rajlaxmisen574@gmail.com>
2021-06-24 00:48:39 +0530

    string interpolation and nested string

---
+++ string_interpolation.py
@@ -0,0 +1,37 @@
+"""
+String interpolation
+
+Question : why 0 is {file_number:03} : ans : 0 is filling the place only
+suppose if you don't pass 0 the it will replace 0 with space
+"""
+
+# normal process
+
+# number_of_files = 3
+
+# for file_number in range(1, number_of_files + 1):
+# 	print(f"image{file_number:03}.png")
+
+# nested process
+number_of_files = 10
+number_digits = int(input("How many digits are used in the numbering scheme? "))
+
+for file_number in range(1, number_of_files + 1):
+	print(f"image{file_number:{number_digits}}.png")
+#Nested string interpolation with format
+number_of_files = 3
+number_digits = int(input("How many digits are used in the numbering scheme? "))
+
+for file_number in range(1, number_of_files + 1):
+	print("image{:0{}}.png".format(file_number, number_digits))
+
+####################
+number_of_files = 3
+number_digits = int(input("How many digits are used in the numbering scheme? "))
+
+for file_number in range(1, number_of_files + 1):
+	print("image{number:0{padding_amount}}.png".format(
+		number=file_number,
+		padding_amount=number_digits
+	))
+
